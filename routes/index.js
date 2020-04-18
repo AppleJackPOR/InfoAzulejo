0e269d2b677fe2baff0611951e7a0c7fbed34361
Hugo Varela <48072291+AppleJackPOR@users.noreply.github.com>
2020-04-18 22:28:21 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var InfoAzulejoDAO = require("../models/InfoAzulejoDAO");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
